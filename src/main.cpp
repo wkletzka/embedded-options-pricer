7772482350424c3c113cca77b8d43738e2dadaee
wkletzka <wkletzka@umich.edu>
2026-08-16 02:20:31 -0400

    Connecting to hardware and LCD

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
 
 // put function declarations here:
-int myFunction(int, int);
+
+// Initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  // Set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  
+  // Print a message to the LCD
+  lcd.setCursor(0, 0);
+  lcd.print("Options Pricer");
+  
+  lcd.setCursor(0, 1);
+  lcd.print("This gon be good");
 }
 
 void loop() {
